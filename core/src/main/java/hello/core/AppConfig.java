9881fd448908b38c64be27cd08a46ebf6837046b
Jisoo Park <qkr1wltn@naver.com>
2023-04-14 17:46:17 +0900

    예제 만들기 - 객체 지향 원리 적용

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,34 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+// AppConfig는 애플리케이션이 실제 동작에 필요한 구현 객체를 생성함
+public class AppConfig {
+    // 역할과 구현 클래스가 한 눈에 들어오게 리팩토링
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+}
