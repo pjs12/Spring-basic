82f9499de747e3c2dc84b773e5e7b72508e34b0b
KTDS 박지수 <jisoo.park2@linakorea.com>
2023-04-20 17:56:18 +0900

    싱글톤 컨테이너1

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -17,13 +17,16 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     // 역할과 구현 클래스가 한 눈에 들어오게 리팩토링
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orserService -> new MemoryMemberRepository()
+
     @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public static MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
